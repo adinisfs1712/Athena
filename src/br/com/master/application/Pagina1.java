5b3bf1ff2e3b3b12eb3bf9977ff7ceffc49ddb6c
adinis1712 <dinis.ferreira.souza@gmail.com>
2021-10-04 21:39:06 -0300

    Nova pagina 1

--- src/br/com/master/application/Pagina1.java
+++ src/br/com/master/application/Pagina1.java
@@ -1,10 +1,13 @@
 package br.com.master.application;
 
+import br.com.master.entities.Cliente;
+
 public class Pagina1 {
 
 	public static void main(String[] args) {
 
-
+		System.out.println("primeira pagina");
+		Cliente cliente = new Cliente();
 	}
 
 }
